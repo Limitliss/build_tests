52a0a1c7f785ebe436321f3cde2d4803752bda95
billybonks <sebastienstettler@gmail.com>
2025-04-07 18:05:08 +0800

    ensure second job is always cancelled

--- test_add.js
+++ test_add.js
@@ -1,14 +1,26 @@
 const add = require("./add");
 
-function testAdd() {
+async function testAdd() {
   if (process.versions.node.split(".")[0] === "22") {
     throw "OMG ERROR";
+  } else {
+    await sleepABit();
   }
   console.assert(add(1, 2) === 3, "Test Case 1 Failed");
   console.assert(add(-1, 1) === 0, "Test Case 2 Failed");
   console.assert(add(0, 0) === 0, "Test Case 3 Failed");
   console.assert(add(2, 2) === 4, "Test Case 4 Failed");
   console.log("All test cases pass");
+}
+
+function sleepABit() {
+  console.log("going to sleep");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("woke up");
+      resolve();
+    }, 1000);
+  });
 }
 
 testAdd();
